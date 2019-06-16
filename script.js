24e6b124deb956bdcedd9747cea23abf68a177e3
Patryk Sanocki <sanocki.patryk@gmail.com>
2019-06-16 23:19:54 +0200

    featured cyclist and some improvements to styling and animations

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
 const arrowUp = document.querySelector("a.go-up.hidden");
+const rankingSection = document.querySelector("#ranking");
+const newsSection = document.querySelector("#news");
+
+const newsElements = document.querySelectorAll(
+  "section.news div.news-panel div.holder div"
+);
+const rankingBars = document.querySelectorAll(
+  "section.ranking div.ranking-wrap ul li div"
+);
 let scrollTop = 0;
+
+handleArrowUp = () => {
+  console.log(scrollTop);
+  if (scrollTop < window.innerHeight / 3) {
+    arrowUp.classList.add("hidden");
+  } else if (
+    scrollTop > window.innerHeight / 3 &&
+    arrowUp.classList.contains("hidden")
+  ) {
+    arrowUp.classList.remove("hidden");
+  }
+};
+
+handleNewsAnimate = () => {
+  if (scrollTop > newsSection.offsetTop - window.screen.height / 3) {
+    newsElements.forEach(news => news.classList.add("animate"));
+  } else if (
+    scrollTop === 0 &&
+    newsElements.item(0).classList.contains("animate")
+  ) {
+    newsElements.forEach(news => news.classList.remove("animate"));
+  }
+};
+
+handleRankingAnimate = () => {
+  if (scrollTop > rankingSection.offsetTop - window.screen.height / 3) {
+    rankingBars.forEach(bar => bar.classList.add("animate"));
+  } else if (
+    scrollTop < rankingSection.offsetTop - window.screen.height &&
+    rankingBars.item(0).classList.contains("animate")
+  ) {
+    rankingBars.forEach(bar => bar.classList.remove("animate"));
+  }
+};
 
 handleScrollArrowUp = () => {
   scrollTop =
@@ -8,11 +51,9 @@ handleScrollArrowUp = () => {
       : (document.documentElement || document.body.parentNode || document.body)
           .scrollTop;
 
-  if (scrollTop > window.innerHeight / 3) {
-    arrowUp.classList.remove("hidden");
-  } else {
-    arrowUp.classList.add("hidden");
-  }
+  handleArrowUp();
+  handleNewsAnimate();
+  handleRankingAnimate();
 };
 
 document.addEventListener("scroll", handleScrollArrowUp);
